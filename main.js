115c8d8e281085243f2f789a3f5198a8b78f0d66
Patryk Wierzbowski <pwierzbowski@sii.pl>
2024-07-05 12:09:00 +0200

    adding letter to word array when pressed key

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 const wordleContainer = document.querySelector(".container__word");
 
-const word = []
+const word = [];
 
 document.addEventListener('keyup', (e) => {
-  if (word.length < 5) {
-    checkKey(e.key)
+  if (e.key === 'Backspace') {
+    word.pop();
   };
 
-  console.log(word)
+  if (word.length < 5 && checkKey(e.key)) {
+    word.push(e.key);
+  };
+
+  console.log(word);
 });
 
 function checkKey(value) {
   if (
-    ((value.charCodeAt(0) >= 65
-    && value.charCodeAt(0) <= 90)
+    ((value.charCodeAt(0) >= 65 && value.charCodeAt(0) <= 90)
     || 
-    (value.charCodeAt(0) >= 97
-    && value.charCodeAt(0) <= 122))
+    (value.charCodeAt(0) >= 97 && value.charCodeAt(0) <= 122))
     && value.length === 1
   ) {
-    console.log(value)
-  }
-}
+    return value;
+  };
+};
+
