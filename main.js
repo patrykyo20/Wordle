d0eb6abf7a2ba03dea6190e724e0a5202f1c5f3d
Patryk Wierzbowski <pwierzbowski@sii.pl>
2024-07-05 11:58:19 +0200

    create main.js file

---
+++ main.js
@@ -0,0 +1,22 @@
+const wordleContainer = document.querySelector(".container__word");
+
+const word = []
+
+document.addEventListener('keyup', (e) => {
+  if (word.length < 5) {
+    checkKey(e.key)
+  };
+
+  console.log(word)
+});
+
+function checkKey(value) {
+  if (
+    value.charCodeAt(0) >= 65
+    && value.charCodeAt(0) <= 90
+    && value.charCodeAt(0) >= 97
+    && value.charCodeAt(0) <= 122
+  ) {
+
+  }
+}
